16deb0a401d30ab9246a9cb64f57d43730423b86
Ilham Hadi Prabawa <iprabawa9@gmail.com>
2019-01-03 12:31:10 +0700

    added side menu drawer

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -5,60 +5,101 @@ const startTabs = () => {
 
     Promise.all([
        Icon.getImageSource("ios-share", 30),
-       Icon.getImageSource("md-map", 30)
+       Icon.getImageSource("md-map", 30),
+       Icon.getImageSource("ios-menu", 30)
     ]).then(sources => {
         Navigation.setRoot({
             root: {
-                bottomTabs: {
-                  children: [{
-                    stack: {
-                        children: [{
-                            component: {
-                                name: 'awesome-places.SharedPlaceScreen',
-                            }
-                         }],
-                        options: {
-                            bottomTab: {
-                                text: 'SharedPlace',
-                                testID: 'FIRST_TAB_BAR_BUTTON',
-                                icon: sources[0]
-                            },
-                            topBar: {
-                                title: {
-                                    text: 'Share Place'
+                sideMenu: {
+                    left: {
+                        component: {
+                            name: 'awesome-places.SideDrawerScreen',  
+                        }
+                    },
+                    center: {
+                        bottomTabs: {
+                            options: {
+                                topBar:{
+                                    visible: true,
                                 }
-                            }
-                      }
-                    }
-                  },
-                  {
-                    stack: {
-                        children: [{
-                            component: {
-                                name: 'awesome-places.FindPlaceScreen', 
-                            }
-                        }],
-                        options: {
-                            bottomTab: {
-                                text: 'FindPlace',
-                                testID: 'SECOND_TAB_BAR_BUTTON',
-                                icon: sources[1]
                             },
-                            topBar: {
-                                title: {
-                                    text: 'Find Place'
+                            children: [{
+                              stack: {
+                                  children: [{
+                                      component: {
+                                          name: 'awesome-places.SharedPlaceScreen',
+                                          options:{
+                                              topBar:{
+                                                leftButtons: [
+                                                    {
+                                                      id: 'sidemenu',
+                                                      icon: sources[2]
+                                                    }
+                                                  ],
+                                                visible: true,
+                                                title: {
+                                                    text: 'Share Place'
+                                                }                                            
+                                              },                                             
+                                          }
+                                      }
+                                   }],
+                                  options: {
+                                      bottomTab: {
+                                          text: 'SharedPlace',
+                                          testID: 'FIRST_TAB_BAR_BUTTON',
+                                          icon: sources[0]
+                                      },
+                                      topBar: {
+                                          visible: true,
+                                          title: {
+                                              text: 'Share Place'
+                                          },
+                                          leftButtons: [
+                                            {
+                                              id: 'buttonOne',
+                                              icon: sources[2]
+                                            }
+                                          ],
+                                      }
                                 }
-                            }
+                              }
+                            },
+                            {
+                              stack: {
+                                  children: [{
+                                      component: {
+                                          name: 'awesome-places.FindPlaceScreen', 
+                                          options: {
+                                              topBar: {
+                                                  visible: true,
+                                                  title: {
+                                                      text: 'Find Place'
+                                                  }
+                                              }
+                                          }
+                                      }
+                                  }],
+                                  options: {
+                                      bottomTab: {
+                                          text: 'FindPlace',
+                                          testID: 'SECOND_TAB_BAR_BUTTON',
+                                          icon: sources[1]
+                                      },
+                                      topBar: {
+                                          title: {
+                                              text: 'Find Place'
+                                          }
+                                      }
+                                    }
+                                }
+                            }]
                           }
+                      },
                     }
-                    
-                  }]
                 }
-              }
-        });  
-    });
-
-    
+            });  
+        });
 }
 
 export default startTabs;
