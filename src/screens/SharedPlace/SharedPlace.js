269b7ba799012350e14ed60bce9d97b4d0b1beb7
Ilham Hadi Prabawa <iprabawa9@gmail.com>
2019-01-04 09:49:20 +0700

    added toggle action

--- src/screens/SharedPlace/SharedPlace.js
+++ src/screens/SharedPlace/SharedPlace.js
@@ -5,7 +5,43 @@ import {connect} from 'react-redux';
 import PlaceInput from '../../components/PlaceInput';
 import { addPlace } from '../../store/actions/index';
 
+import { Navigation } from 'react-native-navigation';
+
 class SharedPlaceScreen extends Component {
+
+    constructor(){
+        super();
+        global.sideMenuVisible = false
+    }
+
+    componentDidMount() {
+        this.navigationEventListener = Navigation.events().bindComponent(this);
+    }
+    
+    componentWillUnmount() {
+       // Not mandatory
+        if (this.navigationEventListener) {
+            this.navigationEventListener.remove();
+        }
+    }
+
+    navigationButtonPressed({buttonId}) {
+
+        switch(buttonId){
+            case 'sidemenu':
+                sideMenuVisible = !sideMenuVisible
+                Navigation.mergeOptions(this.props.componentId, {
+                    sideMenu: {
+                        left: {
+                            visible: sideMenuVisible
+                        }
+                    }
+                });
+                break
+            default:
+                break
+        }
+    }
 
     placeAddedHandler = (placeName) => {
         this.props.onAddPlace (placeName);
